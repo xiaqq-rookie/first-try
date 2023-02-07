387d9e6f1f0df00b346b65540314cd9025f6abc1
xiaqq-rookie <xiaqian9814@163.com>
2023-02-07 17:49:10 +0800

    first-try

---
+++ src/test/java/com/xia/firsttry/FirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xia.firsttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
