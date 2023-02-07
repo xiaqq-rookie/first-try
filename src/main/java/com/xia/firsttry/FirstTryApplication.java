05b6fcae51d05a39767a4270de1dd5e828c794c3
xiaqq-rookie <xiaqian9814@163.com>
2023-02-07 17:30:25 +0800

    first-try

---
+++ src/main/java/com/xia/firsttry/FirstTryApplication.java
@@ -0,0 +1,13 @@
+package com.xia.firsttry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTryApplication.class, args);
+    }
+
+}
